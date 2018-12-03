ae99e7ba5816ac80226bca69545c8fa2fe8dc534
wyt2422762 <wyt2422762@users.noreply.github.com>
2018-12-03 08:55:25 +0800

    代码格式化

--- study1/mySQL.js
+++ study1/mySQL.js
@@ -1,60 +1,62 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'wyt',
-  password : 'zjh222666',
-  database : 'kspace'
+    host: 'localhost',
+    user: 'wyt',
+    password: 'zjh222666',
+    database: 'kspace'
 });
 
-var query = function(){
-	connection.connect();
+var query = function () {
+    connection.connect();
 
-	//查询 
-	connection.query('SELECT * FROM FUND', function (error, results, fields) {
-	  if (error){
-	  	throw error;
-	  }
-	  for(var res in results){
-	  	console.log("id: " + results[res].id + " code: " + results[res].code + " name: " + results[res].name);
-	  }
-	});
-	connection.end();
+    //查询
+    connection.query('SELECT * FROM FUND', function (error, results, fields) {
+        if (error) {
+            throw error;
+        }
+        for (var res in results) {
+            console.log("id: " + results[res].id + " code: " + results[res].code + " name: " + results[res].name);
+        }
+    });
+    connection.end();
 }
 
 //添加
-var add = function(){
-	connection.connect();
-	var addSql = "INSERT INTO jcr_journal_cus (edtion_id, issn, title, jif) VALUES (?,?,?,?)";
-	var addSqlParams = ['2','1005-3028','自定义测试2','3'];
+var add = function () {
+    connection.connect();
+    var addSql = "INSERT INTO jcr_journal_cus (edtion_id, issn, title, jif) VALUES (?,?,?,?)";
+    var addSqlParams = ['2', '1005-3028', '自定义测试2', '3'];
 
-	//开启事务
-	connection.beginTransaction(function(err) {
-		if (err) { throw err; }
-		connection.query(addSql,addSqlParams,function (err, result) {
-		        if(err){
-		        	console.log('[INSERT ERROR] - ',err.message);
-		        	connection.rollback(function() {
-	          		throw err;
-	          	});
-		        	return;
-		        }
-		       console.log('--------------------------INSERT----------------------------');
-		       //console.log('INSERT ID:',result.insertId);        
-		       console.log('INSERT ID:',result);        
-		       console.log('-----------------------------------------------------------------\n\n');  
-		});
-		
-		connection.commit(function(err) {
-	  	if (err) { 
-	    	connection.rollback(function() {
-	      	throw err;
-	    	});
-	  	}
-	    console.log('success!');
-	  });
-	});
-	connection.end();
+    //开启事务
+    connection.beginTransaction(function (err) {
+        if (err) {
+            throw err;
+        }
+        connection.query(addSql, addSqlParams, function (err, result) {
+            if (err) {
+                console.log('[INSERT ERROR] - ', err.message);
+                connection.rollback(function () {
+                    throw err;
+                });
+                return;
+            }
+            console.log('--------------------------INSERT----------------------------');
+            //console.log('INSERT ID:',result.insertId);
+            console.log('INSERT ID:', result);
+            console.log('-----------------------------------------------------------------\n\n');
+        });
+
+        connection.commit(function (err) {
+            if (err) {
+                connection.rollback(function () {
+                    throw err;
+                });
+            }
+            console.log('success!');
+        });
+    });
+    connection.end();
 }
 
 //add();
--- study1/requestHandler.js
+++ study1/requestHandler.js
@@ -1,54 +1,54 @@
 var fs = require("fs");
 
-function index(request, response){
-	console.log("handler1");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("index");
-	response.end();
+function index(request, response) {
+    console.log("handler1");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("index");
+    response.end();
 }
 
 
-function handler1(request, response){
-	console.log("handler1");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("handler1");
-	response.end();
+function handler1(request, response) {
+    console.log("handler1");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("handler1");
+    response.end();
 }
 
-function handler2(request, response){
-	console.log("handler2");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("handler2");
-	response.end();
+function handler2(request, response) {
+    console.log("handler2");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("handler2");
+    response.end();
 }
 
 // 异步读取文件
-function readFileAsync(request, response){
-	fs.readFile('fileTest.txt', function (err, data) {
-   if (err) {
-      response.writeHead(500, {"Content-Type": "text/plain;charset=utf-8"});
-			response.write("读取文件发生错误");
-			response.end();
-   }
-   response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-	 response.write(data);
-	 response.end();
-   console.log("异步读取: " + data.toString());
-});
+function readFileAsync(request, response) {
+    fs.readFile('fileTest.txt', function (err, data) {
+        if (err) {
+            response.writeHead(500, {"Content-Type": "text/plain;charset=utf-8"});
+            response.write("读取文件发生错误");
+            response.end();
+        }
+        response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+        response.write(data);
+        response.end();
+        console.log("异步读取: " + data.toString());
+    });
 }
 
 //异步写入文件
-function writeFileAsync(request, response){
+function writeFileAsync(request, response) {
 
-	fs.writeFile("fileTest.txt", "我是通过写入的文件内容！", function(error){
-		if(error){
-			console.log("写入文件失败");
-		}
+    fs.writeFile("fileTest.txt", "我是通过写入的文件内容！", function (error) {
+        if (error) {
+            console.log("写入文件失败");
+        }
         response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
         response.write("写入文件成功");
         response.end();
-		console.log("写入文件成功");
-	});
+        console.log("写入文件成功");
+    });
 }
 
 exports.index = index;
